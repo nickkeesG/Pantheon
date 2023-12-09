a31e16c6f06fe9f39bf9fb5fc17941df9e265e34
nickkeesG <nkdupuis@protonmail.com>
2023-12-09 14:45:55 +0100

    refactored base daemon to use templates

--- src/daemons/BaseDaemon.ts
+++ src/daemons/BaseDaemon.ts
@@ -1,50 +1,59 @@
 import {GenerateBaseComments} from '../LLMHandler';
 import { Comment, Idea } from '../redux/textSlice';
 
-const baseTemplate = '# Brainstorming';
-const ideaTemplate = '-';
-const commentTemplate = '  -';
+const baseTemplate = '# Brainstorming\n{}';
+const ideaTemplate = '-{}';
+const commentTemplate = '  -[{}]:{}';
 
 
 class BaseDaemon {
-
-
   async generateComment(pastIdeas: Idea[], currentIdeas: Idea[], commentsForPastIdeas: Record<number, Comment[]>, openaiKey: string, openaiOrgId: string) {
-    var context = baseTemplate;
+    let context = "";
 
     for (let i = 0; i < pastIdeas.length; i++) {
-      context += '\n' + ideaTemplate + ' ' + pastIdeas[i].text;
-      
-      var comments = commentsForPastIdeas[pastIdeas[i].id] || [];
+      context += '\n' + ideaTemplate.replace("{}", pastIdeas[i].text);
+
+      let comments = commentsForPastIdeas[pastIdeas[i].id] || [];
       for (let j = 0; j < comments.length; j++) {
-        context += '\n' + commentTemplate + ' [' + comments[j].daemonName + "]: " + comments[j].text;
+        context += '\n' + commentTemplate.replace("{}", comments[j].daemonName).replace("{}", comments[j].text);
       }
     }
     
     // Pick a random current idea
     var randomIndex = Math.floor(Math.random() * currentIdeas.length);
     for (let i = 0; i < randomIndex + 1; i++) {
-      context += '\n' + ideaTemplate + ' ' + currentIdeas[i].text;
+      context += '\n' + ideaTemplate.replace("{}", currentIdeas[i].text);
     }
 
-    context += '\n' + commentTemplate + ' ['; 
+    const commentPrefix= commentTemplate.substring(0, commentTemplate.indexOf("{}"));
+    context += "\n" + commentPrefix;
+    context = baseTemplate.replace("{}", context);
     console.log(context);
 
     var response = await GenerateBaseComments(context, openaiKey, openaiOrgId);
-    console.log(context + response);
-    
-    // Adjust the regex to account for the brackets
-    const splitResponse = response[0].split(/]:\s*(.+)/);
-    if (splitResponse.length < 2) {
-      return null;
-    }
-    const [daemonNameWithBrackets, content] = splitResponse;
-    // Remove the brackets from the daemon name
-    const daemonName = daemonNameWithBrackets.replace('[', '').replace(']', '').trim();
+    console.log(context + response[0]);
 
-    if(daemonName === "" || content === "") {
+    let commentTemplateDivider = "";
+    const regex = /\{\}(.*?)\{\}/;
+    const match = commentTemplate.match(regex);
+
+    if (match && match[1]) {
+        commentTemplateDivider = match[1];
+    }
+    else {  
+        console.log("Error: Regex failed to match");
+        return null;
+    }
+
+    var splitResponse = response[0].split(commentTemplateDivider);
+
+    if (splitResponse.length < 2) {
+      console.log("Error: Response did not contain divider");
       return null;
     }
+
+    var daemonName = splitResponse[0].trim();
+    var content = splitResponse[1].trim();
 
     console.log(daemonName);
     console.log(content);

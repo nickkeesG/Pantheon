8d501b080ac41cdd17037bf6ef308167bbbe519a
sofvanh <23138848+sofvanh@users.noreply.github.com>
2024-03-31 14:27:13 +0300

    Clean up and update dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(
@@ -19,8 +18,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

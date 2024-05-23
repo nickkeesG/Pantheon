d11df0d1eac4ee75e9b1fc029f1a9bdba204dfbd
nickkeesG <nkdupuis@protonmail.com>
2024-05-23 11:50:08 +0100

    removed redundant code in instruct daemon

--- src/daemons/instructDaemon.ts
+++ src/daemons/instructDaemon.ts
@@ -1,5 +1,6 @@
 import { Idea, InstructDaemonConfig } from '../redux/models';
 import { CallChatModel } from '../llmHandler';
+import ChatDaemon from './chatDaemon';
 
 /*
   Used to respond directly to instructions, for performing simple tasks (e.g. summarization)
@@ -15,9 +16,8 @@ class InstructDaemon {
 
     let userPrompt = this.config.userPrompt;
 
-    let pastIdeaText = pastIdeas.map(idea => idea.text).join('\n');
-    userPrompt = userPrompt.replace("{PAST}", pastIdeaText);
-    userPrompt = userPrompt.replace("{CURRENT}", instruction);
+    let pastIdeasText = pastIdeas.map(idea => idea.text).join('\n');
+    userPrompt = ChatDaemon.fillInPrompt(userPrompt, pastIdeasText, instruction);
 
     let response = await CallChatModel(this.config.systemPrompt, userPrompt, openaiKey, openaiOrgId, instructModel);
     return response;

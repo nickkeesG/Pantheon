81e9c55808307faedb79edadd02e1c1e9497f12d
nickkeesG <nkdupuis@protonmail.com>
2024-06-26 16:28:52 +0100

    longer welcome message

--- src/components/WelcomeMessage.tsx
+++ src/components/WelcomeMessage.tsx
@@ -31,6 +31,9 @@ const WelcomeMessage = () => {
           <WelcomePanel>
             <WelcomeHeader>Welcome to Pantheon</WelcomeHeader>
             <p>Pick a topic you would like to make progress on, and make an effort to think out loud, writing out your thoughts as they appear. Daemons will appear to the left and right of your thoughts offering commentary.</p>
+            <p>Press ENTER after each of your thoughts, so they can be added to the context. Try not to wait for the daemons to respond, and treat the page more as a diary or some personal notes. They will share their thoughts when they are ready!</p>
+            <p>It is not possible to go back and edit/delete your thoughts, but you can branch your thoughts at any point by clicking the little plus icon next to a thought.</p>
+            <p>Finally, if you would like a fresh page, you can either create a new section of the tree by clicking "New Section," or by creating a completely new tree by clicking the icon in the top left.</p>
           </WelcomePanel>
         </Modal>
       )

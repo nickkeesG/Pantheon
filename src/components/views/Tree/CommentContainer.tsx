57406c488bb9d50b7f46d3eb953f73b28c6ae4ec
sofvanh <23138848+sofvanh@users.noreply.github.com>
2024-08-13 13:54:52 +0530

    'Copy comment' button

--- src/components/views/Tree/CommentContainer.tsx
+++ src/components/views/Tree/CommentContainer.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
+import React, { MouseEvent, useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Comment } from '../../../redux/models';
-import { IconButtonSmall, TextButton } from '../../../styles/sharedStyles';
-import { IoIosThumbsUp } from "react-icons/io";
-// import { useAppDispatch } from '../../../hooks';
-// import { approveComment } from '../../../redux/commentSlice';
+import { ContainerHorizontal, IconButtonSmall, TextButton } from '../../../styles/sharedStyles';
+import { FiCheckCircle, FiCopy } from 'react-icons/fi';
 import { aiFont, fadeInAnimation } from '../../../styles/mixins';
 import { useModal } from '../../ModalContext';
 import CommentContext from './CommentContext';
 
 
 const CommentName = styled.div`
-  text-align: right;
   color: var(--accent-color-coral);
 `;
 
@@ -20,23 +17,8 @@ const CommentText = styled.div`
   ${fadeInAnimation};
 `;
 
-
-interface ThumbsUpIconProps {
-  userApproved: boolean;
-  [key: string]: any;
-}
-
-const ThumbsUpIcon = ({ userApproved, ...props }: ThumbsUpIconProps) => <IoIosThumbsUp {...props} />;
-
-const ThumbsUpButton = styled(IconButtonSmall).attrs({ as: ThumbsUpIcon }) <{ userApproved: boolean }>`
-  position: absolute;
-  top: 2px;
-  left: 8px;
-  color: ${props => props.userApproved ? 'var(--accent-color-coral)' : 'var(--line-color)'};
-  pointer-events: ${props => props.userApproved ? 'none' : 'auto'};
-  cursor: ${props => props.userApproved ? 'default' : 'pointer'};
-  opacity: ${props => props.userApproved ? '0.7' : '1'};
-  transition: opacity 0.3s ease;
+const CopyButton = styled(IconButtonSmall)`
+  margin-left: auto;
 `;
 
 const StyledCommentContainer = styled(TextButton)`
@@ -47,33 +29,63 @@ const StyledCommentContainer = styled(TextButton)`
   color: var(--text-color-secondary);
   ${aiFont};
 
-  ${ThumbsUpButton} {
+  &:active:not(:disabled) {
+    opacity: 1;
+  }
+
+  &:active:not(:disabled):not(:has(${CopyButton}:active)) {
+    opacity: 70%;
+  }
+
+  ${CopyButton} {
     opacity: 0;
     transition: opacity 0.3s ease;
   }
 
-  &:hover ${ThumbsUpButton} {
+  &:hover ${CopyButton} {
     opacity: 1;
+  }
+
+  ${CopyButton}:active:not(:disabled) {
+    opacity: 70%;
   }
 `;
 
 
 const CommentContainer: React.FC<{ comment: Comment }> = ({ comment }) => {
-  // const dispatch = useAppDispatch();
+  const [isCopied, setIsCopied] = useState(false);
   const { addModal } = useModal();
+
+  const copy = useCallback(async (event: MouseEvent) => {
+    event.stopPropagation();
+    try {
+      await navigator.clipboard.writeText(comment.text);
+      setIsCopied(true);
+    } catch (err) {
+      console.error("Failed to copy comment to clipboard", err)
+    }
+  }, [comment.text]);
+
+  useEffect(() => {
+    if (isCopied) {
+      const timer = setTimeout(() => {
+        setIsCopied(false);
+      }, 2000);
+      return () => clearTimeout(timer);
+    }
+  }, [isCopied]);
 
   return (
     <div style={{ margin: '4px' }}>
       <StyledCommentContainer onClick={() => addModal(<CommentContext comment={comment} />)}>
-        <CommentName>{comment.daemonName}</CommentName>
+        <ContainerHorizontal style={{ alignItems: 'center' }}>
+          <CommentName>{comment.daemonName}</CommentName>
+          <CopyButton
+            onClick={copy}>
+            {isCopied ? <FiCheckCircle /> : <FiCopy />}
+          </CopyButton>
+        </ContainerHorizontal>
         <CommentText>{comment.text}</CommentText>
-        {/* <ThumbsUpButton
-          userApproved={comment.userApproved}
-          onClick={(event: React.MouseEvent) => {
-            event.stopPropagation();
-            dispatch(approveComment(comment.id));
-          }}
-        /> */}
       </StyledCommentContainer>
     </div>
   );

254cd33e88f47b0a2c7ccc58b896808f1de8a76d
sofvanh <23138848+sofvanh@users.noreply.github.com>
2026-02-13 14:28:14 -0800

    fix: button within button react warning

--- src/components/views/Tree/CommentContainer.tsx
+++ src/components/views/Tree/CommentContainer.tsx
@@ -1,16 +1,16 @@
-import React, { MouseEvent, useCallback, useEffect, useState } from 'react';
-import styled from 'styled-components';
-import { Comment } from '../../../redux/models';
-import { ContainerHorizontal, IconButtonSmall, TextButton } from '../../../styles/sharedStyles';
-import { FiCheckCircle, FiCopy } from 'react-icons/fi';
-import { aiFont, fadeInAnimation } from '../../../styles/mixins';
-import { useModal } from '../../ModalContext';
-import CommentContext from './CommentContext';
-
-
-const CommentName = styled.div`
-  color: var(--accent-color-coral);
-`;
+import type React from "react";
+import { type MouseEvent, useCallback, useEffect, useState } from "react";
+import { FiCheckCircle, FiCopy } from "react-icons/fi";
+import styled from "styled-components";
+import type { Comment } from "../../../redux/models";
+import {
+	aiFont,
+	fadeInAnimation,
+	highlightOnHover,
+} from "../../../styles/mixins";
+import { IconButtonSmall } from "../../../styles/sharedStyles";
+import { useModal } from "../../ModalContext";
+import CommentContext from "./CommentContext";
 
 const CommentText = styled.div`
   text-align: left;
@@ -21,19 +21,18 @@ const CopyButton = styled(IconButtonSmall)`
   margin-left: auto;
 `;
 
-const StyledCommentContainer = styled(TextButton)`
+const StyledCommentContainer = styled.div`
   width: 100%;
   position: relative;
   padding: 6px 12px;
   margin: 0px;
   color: var(--text-color-secondary);
+  cursor: pointer;
+  border-radius: 8px;
+  ${highlightOnHover};
   ${aiFont};
 
-  &:active:not(:disabled) {
-    opacity: 1;
-  }
-
-  &:active:not(:disabled):not(:has(${CopyButton}:active)) {
+  &:active:not(:has(${CopyButton}:active)) {
     opacity: 70%;
   }
 
@@ -51,44 +50,49 @@ const StyledCommentContainer = styled(TextButton)`
   }
 `;
 
-
 const CommentContainer: React.FC<{ comment: Comment }> = ({ comment }) => {
-  const [isCopied, setIsCopied] = useState(false);
-  const { addModal } = useModal();
+	const [isCopied, setIsCopied] = useState(false);
+	const { addModal } = useModal();
 
-  const copy = useCallback(async (event: MouseEvent) => {
-    event.stopPropagation();
-    try {
-      await navigator.clipboard.writeText(comment.text);
-      setIsCopied(true);
-    } catch (err) {
-      console.error("Failed to copy comment to clipboard", err)
-    }
-  }, [comment.text]);
+	const copy = useCallback(
+		async (event: MouseEvent) => {
+			event.stopPropagation();
+			try {
+				await navigator.clipboard.writeText(comment.text);
+				setIsCopied(true);
+			} catch (err) {
+				console.error("Failed to copy comment to clipboard", err);
+			}
+		},
+		[comment.text],
+	);
 
-  useEffect(() => {
-    if (isCopied) {
-      const timer = setTimeout(() => {
-        setIsCopied(false);
-      }, 2000);
-      return () => clearTimeout(timer);
-    }
-  }, [isCopied]);
+	useEffect(() => {
+		if (isCopied) {
+			const timer = setTimeout(() => {
+				setIsCopied(false);
+			}, 2000);
+			return () => clearTimeout(timer);
+		}
+	}, [isCopied]);
 
-  return (
-    <div style={{ margin: '4px' }}>
-      <StyledCommentContainer onClick={() => addModal(<CommentContext comment={comment} />)}>
-        <ContainerHorizontal style={{ alignItems: 'center' }}>
-          <CommentName>{comment.daemonName}</CommentName>
-          <CopyButton
-            onClick={copy}>
-            {isCopied ? <FiCheckCircle /> : <FiCopy />}
-          </CopyButton>
-        </ContainerHorizontal>
-        <CommentText>{comment.text}</CommentText>
-      </StyledCommentContainer>
-    </div>
-  );
+	return (
+		<div className="m-1">
+			<StyledCommentContainer
+				onClick={() => addModal(<CommentContext comment={comment} />)}
+			>
+				<div className="flex w-full items-center">
+					<span className="text-[var(--accent-color-coral)]">
+						{comment.daemonName}
+					</span>
+					<CopyButton onClick={copy}>
+						{isCopied ? <FiCheckCircle /> : <FiCopy />}
+					</CopyButton>
+				</div>
+				<CommentText>{comment.text}</CommentText>
+			</StyledCommentContainer>
+		</div>
+	);
 };
 
 export default CommentContainer;
